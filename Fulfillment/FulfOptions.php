5d7a859983117b4989102ffb2f42b4721c950e39
unknown <sil3nt2015@gmail.com>
2020-08-25 04:38:21 +0300

    Removed Simple Res Done

--- Fulfillment/FulfOptions.php
+++ Fulfillment/FulfOptions.php
@@ -3,9 +3,7 @@
           <a class="mdl-navigation__link" href="./BrowseAll.php">Browse All Products.</a>
           <a class="mdl-navigation__link" href="./BrowseAvailable.php">Browse Available Products.</a>
           <a class="mdl-navigation__link" href="./Fulfillment.php">Show Order's.</a>
-          <a class="mdl-navigation__link" href="./Requests.php">Show Return/Replacement Requests(Multi).</a>
-          <a class="mdl-navigation__link" href="./Requests.php">Show Return/Replacement Requests(One).</a>
-
+          <a class="mdl-navigation__link" href="./Requests.php">Show Return/Replacement Requests.</a>
           <a class="mdl-navigation__link" href="./Entry/DataEntry.php">Enter New Product.</a>
 
     
